3de9c26c98fc71d362b1f5a5c8c47c29eee34d42
cs4712 <471218750@qq.com>
2017-10-27 17:41:10 -0400

    Initial Commit

---
+++ a3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a3
+//
+//  Created by Shi Cheng on 17/10/27.
+//  Copyright © 2017年 Shi Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
